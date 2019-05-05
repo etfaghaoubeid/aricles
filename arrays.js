808af740ee0e464fd899f9af77492d16799ad105
etfaghaoubeid <etfaghaoubeid@gmail.com>
2019-05-05 20:12:05 +0000

    add reduce method

--- arrays.js
+++ arrays.js
@@ -35,3 +35,28 @@ console.log("lessPersonInfo",
 const filterNumbers = [1, 2, 3, , 4, 5, 6, 7, 8]
 const filterOddNumbers = filterNumbers.filter(number => number % 2 != 0)
 console.log("filterOddNumbers", filterOddNumbers);
+
+/*
+:::::::::::::::::::::::::::
+      reduce function
+:::::::::::::::::::::::::::
+*/
+
+const reduceNumbers = [1, 2, 3, 4, 5, 6, 7, 8];
+const sumReduceNumbers = reduceNumbers.reduce((sum, item) => sum += item, 0);
+console.log("sumReduceNumbers", sumReduceNumbers)
+
+let map = (arr, fn) => {
+        return arr.reduce((mapedArray, element) => {
+                return [...mapedArray, fn(element)]
+
+        }, [])
+}
+console.log(map([1, 2, 3, 4, 5], element => element + 1))
+
+let filter = (arr, fn) => {
+        return arr.reduce((filterElement, element) => {
+                return fn(element) ? [...filterElement, element] : [...filterElement]
+        }, [])
+}
+console.log(filter([1, 2, 3, 4, 5], element => element % 2 != 0))
