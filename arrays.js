19ae58d7aaa8e728367032f3fdcb13e19db526d8
etfaghaoubeid <etfaghaoubeid@gmail.com>
2019-05-15 10:06:05 +0000

    clean Up

--- arrays.js
+++ arrays.js
@@ -1,264 +1,251 @@
-/*
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
- arrays  link = https://medium.freecodecamp.org/what-you-should-know-about-javascript-arrays-a7e5394f78f8?fbclid=IwAR3OnAFjINP6PZRnB2xP-K2NDiT7NnJvS89DHpFLy--bDZ3E7zmI0WJDC7o
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+ let numbers = [1, 2, 3, 4];
+ const doubleNumbers = numbers.map(number => number * 2);
+ console.log("map double numbers", doubleNumbers);
+ // keep particular property
+ let persons = [{
+     name: "Mouvid",
+     age: 23
+   },
+   {
+     name: "Hamoud",
+     job: "software"
+   }
+ ];
+
+ const lessPersonInfo = persons.map(person => person.name);
+ console.log("lessPersonInfo", lessPersonInfo);
+ /*
+ :::::::::::::::::::::::::::
+       filter function
+ :::::::::::::::::::::::::::
  */
-/*
-:::::::::::::::::::::::::::
-      map function
-:::::::::::::::::::::::::::
-*/
+ const filterNumbers = [1, 2, 3, , 4, 5, 6, 7, 8];
+ const filterOddNumbers = filterNumbers.filter(number => number % 2 != 0);
+ console.log("filterOddNumbers", filterOddNumbers);
 
-let numbers = [1, 2, 3, 4];
-const doubleNumbers = numbers.map(number => number * 2);
-console.log("map double numbers", doubleNumbers);
-// keep particular property
-let persons = [
-  {
-    name: "Mouvid",
-    age: 23
-  },
-  {
-    name: "Hamoud",
-    job: "software"
-  }
-];
+ /*
+ :::::::::::::::::::::::::::
+       reduce function
+ :::::::::::::::::::::::::::
+ */
 
-const lessPersonInfo = persons.map(person => person.name);
-console.log("lessPersonInfo", lessPersonInfo);
-/*
-:::::::::::::::::::::::::::
-      filter function
-:::::::::::::::::::::::::::
-*/
-const filterNumbers = [1, 2, 3, , 4, 5, 6, 7, 8];
-const filterOddNumbers = filterNumbers.filter(number => number % 2 != 0);
-console.log("filterOddNumbers", filterOddNumbers);
+ const reduceNumbers = [1, 2, 3, 4, 5, 6, 7, 8];
+ const sumReduceNumbers = reduceNumbers.reduce((sum, item) => (sum += item), 0);
+ console.log("sumReduceNumbers", sumReduceNumbers);
 
-/*
-:::::::::::::::::::::::::::
-      reduce function
-:::::::::::::::::::::::::::
-*/
+ let map = (arr, fn) => {
+   return arr.reduce((mapedArray, element) => {
+     return [...mapedArray, fn(element)];
+   }, []);
+ };
+ console.log(map([1, 2, 3, 4, 5], element => element + 1));
 
-const reduceNumbers = [1, 2, 3, 4, 5, 6, 7, 8];
-const sumReduceNumbers = reduceNumbers.reduce((sum, item) => (sum += item), 0);
-console.log("sumReduceNumbers", sumReduceNumbers);
+ let filter = (arr, fn) => {
+   return arr.reduce((filterElement, element) => {
+     return fn(element) ? [...filterElement, element] : [...filterElement];
+   }, []);
+ };
+ console.log(filter([1, 2, 3, 4, 5], element => element % 2 != 0));
 
-let map = (arr, fn) => {
-  return arr.reduce((mapedArray, element) => {
-    return [...mapedArray, fn(element)];
-  }, []);
-};
-console.log(map([1, 2, 3, 4, 5], element => element + 1));
+ function flatDeep(arr) {
+   return arr.reduce((accumilator, element) => {
+     return Array.isArray(element) ?
+       [...accumilator, ...flatDeep(element)] :
+       [...accumilator, element];
+   }, []);
+ }
 
-let filter = (arr, fn) => {
-  return arr.reduce((filterElement, element) => {
-    return fn(element) ? [...filterElement, element] : [...filterElement];
-  }, []);
-};
-console.log(filter([1, 2, 3, 4, 5], element => element % 2 != 0));
+ console.log("flatDeeper", flatDeep([1, 2, [3, 4, [5], 6], 8]));
 
-function flatDeep(arr) {
-  return arr.reduce((accumilator, element) => {
-    return Array.isArray(element)
-      ? [...accumilator, ...flatDeep(element)]
-      : [...accumilator, element];
-  }, []);
-}
+ /*
+ :::::::::::::::::::::::::::
+       spread operator
+ :::::::::::::::::::::::::::
+ */
+ const numbersSpread = [1, 2, 3, 4, 5];
+ const copynNumbersSpread = [...numbersSpread];
+ console.log("copynNumbersSpread", copynNumbersSpread);
+ const otherNumbers = [6, 7, 8];
+ const concatNumber = [...numbersSpread, ...otherNumbers];
+ console.log("concatNumber", concatNumber);
+ // shallow copy
+ let users = [{
+     name: "atigh",
+     postion: "web developer",
+     age: 29
+   },
+   {
+     name: "Lalle",
+     postion: "Ban6ye",
+     age: 20
+   },
+   3
+ ];
+ let usersCopy = [...users];
+ usersCopy[0].name = "chavie";
+ usersCopy[0].age = 28;
+ usersCopy[2] = 4;
+ console.log(users, usersCopy);
 
-console.log("flatDeeper", flatDeep([1, 2, [3, 4, [5], 6], 8]));
+ /*
+ :::::::::::::::::::::::::::
+       includes
+ :::::::::::::::::::::::::::
+ */
 
-/*
-:::::::::::::::::::::::::::
-      spread operator
-:::::::::::::::::::::::::::
-*/
-const numbersSpread = [1, 2, 3, 4, 5];
-const copynNumbersSpread = [...numbersSpread];
-console.log("copynNumbersSpread", copynNumbersSpread);
-const otherNumbers = [6, 7, 8];
-const concatNumber = [...numbersSpread, ...otherNumbers];
-console.log("concatNumber", concatNumber);
-// shallow copy
-let users = [
-  {
-    name: "atigh",
-    postion: "web developer",
-    age: 29
-  },
-  {
-    name: "Lalle",
-    postion: "Ban6ye",
-    age: 20
-  },
-  3
-];
-let usersCopy = [...users];
-usersCopy[0].name = "chavie";
-usersCopy[0].age = 28;
-usersCopy[2] = 4;
-console.log(users, usersCopy);
+ let includesNumbers = [1, 2, 3, 4];
+ console.log(includesNumbers.includes(2));
 
-/*
-:::::::::::::::::::::::::::
-      includes
-:::::::::::::::::::::::::::
-*/
+ /*
+ :::::::::::::::::::::::::::
+       concat && concatAll
+ :::::::::::::::::::::::::::
+ */
+ let concatNumbers = [1, 2, 3, 4];
+ console.log(concatNumbers.concat([5, 6, 7]));
 
-let includesNumbers = [1, 2, 3, 4];
-console.log(includesNumbers.includes(2));
+ /*
+ :::::::::::::::::::::::::::
+       forEach
+ :::::::::::::::::::::::::::
+ */
 
-/*
-:::::::::::::::::::::::::::
-      concat && concatAll
-:::::::::::::::::::::::::::
-*/
-let concatNumbers = [1, 2, 3, 4];
-console.log(concatNumbers.concat([5, 6, 7]));
+ function concatall(arr, arrays) {
+   return arr.concat(...arrays);
+ }
+ console.log(concatall([1, 2, 3], [3, 4, 5, 6], [7, 8]));
 
-/*
-:::::::::::::::::::::::::::
-      forEach
-:::::::::::::::::::::::::::
-*/
+ let forEachNumbers = [1, 2, 3, 4, 7];
+ forEachNumbers.forEach(console.log);
 
-function concatall(arr, arrays) {
-  return arr.concat(...arrays);
-}
-console.log(concatall([1, 2, 3], [3, 4, 5, 6], [7, 8]));
+ /*
+ :::::::::::::::::::::::::::
+       indexOf
+ :::::::::::::::::::::::::::
+ */
+ let indexOfNumbers = [1, 2, 3, 45];
+ console.log(indexOfNumbers.indexOf(45));
 
-let forEachNumbers = [1, 2, 3, 4, 7];
-forEachNumbers.forEach(console.log);
+ /*
+ :::::::::::::::::::::::::::
+  find stop when find one
+ :::::::::::::::::::::::::::
+ */
+ let findNumbers = [1, 2, 3, 4, 5, 6];
 
-/*
-:::::::::::::::::::::::::::
-      indexOf
-:::::::::::::::::::::::::::
-*/
-let indexOfNumbers = [1, 2, 3, 45];
-console.log(indexOfNumbers.indexOf(45));
+ let r = findNumbers.find(element => element > 3);
+ console.log(r);
 
-/*
-:::::::::::::::::::::::::::
- find stop when find one
-:::::::::::::::::::::::::::
-*/
-let findNumbers = [1, 2, 3, 4, 5, 6];
+ /*
+ :::::::::::::::::::::::::::
+     findIndex return index 
+ :::::::::::::::::::::::::::
+ */
+ let findEndexNumbers = [1, 2, 3, 4, 5, 6, 7];
+ let re = findEndexNumbers.findIndex(element => element == 4);
+ console.log(re);
 
-let r = findNumbers.find(element => element > 3);
-console.log(r);
+ /*
+ :::::::::::::::::::::::::::
+     slice 
+ :::::::::::::::::::::::::::
+ */
+ let sliceNumbers = [1, 2, 3, 4, 5, 67, 8];
+ let sliceCopy = sliceNumbers.slice();
+ console.log(sliceNumbers, sliceCopy);
+ let portion = sliceNumbers.slice(0, 4);
+ console.log(portion);
+ /*
+ :::::::::::::::::::::::::::
+  some:return true or false  
+ :::::::::::::::::::::::::::
+ */
+ let someNumbers = [1.2, 3, 4, 5, 67, 8];
+ let someResult = someNumbers.some(item => item > 8);
+ console.log(someResult);
 
-/*
-:::::::::::::::::::::::::::
-    findIndex return index 
-:::::::::::::::::::::::::::
-*/
-let findEndexNumbers = [1, 2, 3, 4, 5, 6, 7];
-let re = findEndexNumbers.findIndex(element => element == 4);
-console.log(re);
+ /*
+ :::::::::::::::::::::::::::
+  evry : test all item  
+ :::::::::::::::::::::::::::
+ */
+ let evryNumbers = [1, 2, 3, 4, 56, 7, 8];
+ let evryResult = evryNumbers.every(item => item > 0);
+ console.log(evryResult);
 
-/*
-:::::::::::::::::::::::::::
-    slice 
-:::::::::::::::::::::::::::
-*/
-let sliceNumbers = [1, 2, 3, 4, 5, 67, 8];
-let sliceCopy = sliceNumbers.slice();
-console.log(sliceNumbers, sliceCopy);
-let portion = sliceNumbers.slice(0, 4);
-console.log(portion);
-/*
-:::::::::::::::::::::::::::
- some:return true or false  
-:::::::::::::::::::::::::::
-*/
-let someNumbers = [1.2, 3, 4, 5, 67, 8];
-let someResult = someNumbers.some(item => item > 8);
-console.log(someResult);
+ /*
+ :::::::::::::::::::::::::::
+   flat
+ :::::::::::::::::::::::::::
+ */
+ // const flatNumberss = [1, 2, [3, 4, [5, [6, 7]], [[[[8]]]]]];
+ // const numbersflattenOnce = flatNumberss.flat();
+ // console.log(numbersflattenOnce);
 
-/*
-:::::::::::::::::::::::::::
- evry : test all item  
-:::::::::::::::::::::::::::
-*/
-let evryNumbers = [1, 2, 3, 4, 56, 7, 8];
-let evryResult = evryNumbers.every(item => item > 0);
-console.log(evryResult);
+ /*
+ :::::::::::::::::::::::::::
+   join
+ :::::::::::::::::::::::::::
+ */
+ let names = ["Atigh", "Larry", "Musk"];
+ let joinNames = names.join(">");
+ console.log(joinNames);
+ /*
+ :::::::::::::::::::::::::::
+     isArray static
+ :::::::::::::::::::::::::::
+ */
 
-/*
-:::::::::::::::::::::::::::
-  flat
-:::::::::::::::::::::::::::
-*/
-// const flatNumberss = [1, 2, [3, 4, [5, [6, 7]], [[[[8]]]]]];
-// const numbersflattenOnce = flatNumberss.flat();
-// console.log(numbersflattenOnce);
+ let isArrayNumbers = [1, 3, 4, 5, 6];
+ console.log(Array.isArray(isArrayNumbers));
 
-/*
-:::::::::::::::::::::::::::
-  join
-:::::::::::::::::::::::::::
-*/
-let names = ["Atigh", "Larry", "Musk"];
-let joinNames = names.join(">");
-console.log(joinNames);
-/*
-:::::::::::::::::::::::::::
-    isArray static
-:::::::::::::::::::::::::::
-*/
+ /*
+ :::::::::::::::::::::::::::
+     sort 
+ :::::::::::::::::::::::::::
+ */
+ let sortNames = ["Atigh", "Chavie", "Mouvid", "Hamoud"];
+ console.log(sortNames.sort());
+ let sortNumbers = [23, 56, 45, 2, 4, 56, 89, 43];
+ console.log(sortNumbers.sort((a, b) => a - b));
 
-let isArrayNumbers = [1, 3, 4, 5, 6];
-console.log(Array.isArray(isArrayNumbers));
+ /*
+ :::::::::::::::::::::::::::
+     pop
+ :::::::::::::::::::::::::::
+ */
+ let popNumbers = [1, 2, 34, 5];
+ popNumbers.pop();
+ console.log("popNumbers", popNumbers);
+ /*
+ :::::::::::::::::::::::::::
+     fill
+ :::::::::::::::::::::::::::
+ */
 
-/*
-:::::::::::::::::::::::::::
-    sort 
-:::::::::::::::::::::::::::
-*/
-let sortNames = ["Atigh", "Chavie", "Mouvid", "Hamoud"];
-console.log(sortNames.sort());
-let sortNumbers = [23, 56, 45, 2, 4, 56, 89, 43];
-console.log(sortNumbers.sort((a, b) => a - b));
+ /*
+ :::::::::::::::::::::::::::
+     reverce 
+ :::::::::::::::::::::::::::
+ */
+ let reverceNumbers = [1, 2, 3, 4, 5, 6];
+ reduceNumbers.reverse();
+ console.log("reduceNumbers", reduceNumbers);
 
-/*
-:::::::::::::::::::::::::::
-    pop
-:::::::::::::::::::::::::::
-*/
-let popNumbers = [1, 2, 34, 5];
-popNumbers.pop();
-console.log("popNumbers", popNumbers);
-/*
-:::::::::::::::::::::::::::
-    fill
-:::::::::::::::::::::::::::
-*/
+ /*
+ :::::::::::::::::::::::::::
+     shift 
+ :::::::::::::::::::::::::::
+ */
+ let shiftNumbers = [1, 2, 3, 4];
+ shiftNumbers.shift();
+ console.log(shiftNumbers);
 
-/*
-:::::::::::::::::::::::::::
-    reverce 
-:::::::::::::::::::::::::::
-*/
-let reverceNumbers = [1, 2, 3, 4, 5, 6];
-reduceNumbers.reverse();
-console.log("reduceNumbers", reduceNumbers);
-
-/*
-:::::::::::::::::::::::::::
-    shift 
-:::::::::::::::::::::::::::
-*/
-let shiftNumbers = [1, 2, 3, 4];
-shiftNumbers.shift();
-console.log(shiftNumbers);
-
-/*
-:::::::::::::::::::::::::::
-    unshift
-:::::::::::::::::::::::::::
-*/
-let unshiftNumbers = [1, 2, 3, 4, 5];
-unshiftNumbers.unshift(-1, 0);
-console.log(unshiftNumbers);
+ /*
+ :::::::::::::::::::::::::::
+     unshift
+ :::::::::::::::::::::::::::
+ */
+ let unshiftNumbers = [1, 2, 3, 4, 5];
+ unshiftNumbers.unshift(-1, 0);
+ console.log(unshiftNumbers);
